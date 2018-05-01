d5b84db22d289c92458b5f09b7a1c8e23e1a44d4
karthikhacker <karthik.hacker85@gmail.com>
2018-05-01 11:09:39 +0530

    Added multer file uploading functionalty

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 const port = process.env.PORT || 3000;
 const app = express();
-
+//Middleware
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+//routes
+app.post('/upload',upload.single('file'),(req,res) => {
+  if(!req.file){
+    res.json({message:"Select a file"});
+  }
+  res.json(req.file);
+});
 //server
 app.listen(port,() => {
   console.log('App running at port ',port);
